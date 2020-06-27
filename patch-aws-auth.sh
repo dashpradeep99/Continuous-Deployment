86286eb946afc5deab3bfa38f68031239be9c94b
dashpradeep99 <dashpradeep_99@yahoo.com>
2020-06-27 23:40:21 +0530

    Update patch-aws-auth.sh

--- patch-aws-auth.sh
+++ patch-aws-auth.sh
@@ -2,7 +2,7 @@
 
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
-ROLE="    - rolearn: arn:aws:iam::$ACCOUNT_ID:role/EKSDeepDiveCodeBuildKubectlRole\n      username: build\n      groups:\n        - system:masters"
+ROLE="    - rolearn: arn:aws:iam::$ACCOUNT_ID:role/EKSCodeBuildKubectlRole\n      username: build\n      groups:\n        - system:masters"
 
 kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/mapRoles: \|/{print;print \"$ROLE\";next}1" > /tmp/aws-auth-patch.yml
 
